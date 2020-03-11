5ded9881fa17e7baa18033c6a04ccb03d1a7d9be
Alessandre Camargo <alessandre.camargo@gmail.com>
2020-03-10 23:25:42 -0300

    updated: Envio de emails'

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3e00dfa19d8a1e',
+    pass: 'f7632ce9286c38',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
